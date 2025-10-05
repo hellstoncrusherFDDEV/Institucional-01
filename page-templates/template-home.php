8ae7f381ab15180d5125a98b783acb8e492feeb9
Hellston Linhares <hellstonlinhares@hotmail.com>
2025-10-04 21:55:07 -0300

    Update template-home.php

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -25,7 +25,7 @@ get_header(); // Carrega o header.php, incluindo o menu fixo e responsivo
         <hr class="my-4"> 
 
         <center>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/S86zAxbwa3k?si=aOVK7a39DLm64gwY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="80%" src="https://www.youtube.com/embed/S86zAxbwa3k?si=aOVK7a39DLm64gwY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </center>
 
         <p>
