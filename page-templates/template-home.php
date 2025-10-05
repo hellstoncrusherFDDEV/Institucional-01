6ece61cabae6fd57966a98a44ace853d7da13d04
Hellston Linhares <hellstonlinhares@hotmail.com>
2025-10-04 21:50:58 -0300

    Update template-home.php

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -23,6 +23,11 @@ get_header(); // Carrega o header.php, incluindo o menu fixo e responsivo
         </p>
 
         <hr class="my-4"> 
+
+        <center>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/S86zAxbwa3k?si=aOVK7a39DLm64gwY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </center>
+
         <p>
             Ideal para desenvolvedores, pequenos e-commerces e empreendedores que buscam uma solução rápida, confiável e com preço justo.
         </p>
